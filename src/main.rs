49632877400dfb3f6b201a4377222425b38f8fe5
Gustavo Fenilli <gustavofenilli@gmail.com>
2024-11-22 17:31:26 -0300

    feat: adds simple component and system to test ecs ergonomics

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,44 @@
 mod ecs;
 
+use std::{thread, time::Duration};
+
 use ecs::ECS;
 
 #[derive(Debug)]
-pub struct Resource {
-    key: &'static str,
+pub struct PositionComponent {
+    x: f32,
+    y: f32,
 }
 
-impl Resource {
-    pub fn new(key: &'static str) -> Self {
-        Self { key }
+impl PositionComponent {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self { x, y }
+    }
+
+    pub fn from_u32(x: u32, y: u32) -> Self {
+        Self::new(x as f32, y as f32)
+    }
+}
+
+pub fn movement_system(ecs: &ECS) {
+    let entities = ecs.entities();
+
+    for entity in entities {
+        if let Some(position) = ecs.get_component::<PositionComponent>(*entity) {
+            println!("{:?}", position);
+        }
     }
 }
 
 fn main() {
     let mut ecs = ECS::new();
 
-    ecs.register_resource(Resource::new("Hello"));
+    let player = ecs.create_entity();
+    ecs.set_component(player, PositionComponent::from_u32(200, 200));
 
-    let e1 = ecs.create_entity();
-    let e2 = ecs.create_entity();
+    loop {
+        movement_system(&ecs);
 
-    ecs.set_component::<u32>(e1, 10);
-    ecs.set_component::<u32>(e2, 20);
-
-    let resource = ecs.get_resource::<Resource>();
-    for entity in ecs.entities() {
-        if let Some(health) = ecs.get_component::<u32>(*entity) {
-            println!("{} - {:?}", health, resource);
-        }
+        thread::sleep(Duration::from_millis(1000 / 60));
     }
 }
