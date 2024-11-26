268df77e3ef6b1d364aa6cd8224ed06df278454a
Gustavo Fenilli <gustavofenilli@gmail.com>
2024-11-25 22:06:27 -0300

    feat: add simple tick state

--- src/game/states/app_state.rs
+++ src/game/states/app_state.rs
@@ -1,74 +1,29 @@
 use winit::window::Window;
 
-use super::gpu_state::GpuState;
+use super::{gpu_state::GpuState, tick_state::TickState};
 use crate::ecs::ECS;
-
-#[derive(Debug)]
-pub struct PlayerTag;
-
-#[derive(Debug)]
-pub struct PositionComponent {
-    x: u32,
-    y: u32,
-}
-
-impl PositionComponent {
-    pub fn new(x: u32, y: u32) -> Self {
-        Self { x, y }
-    }
-}
-
-struct Ticker {
-    ticks: u32,
-}
-
-fn update_player(ecs: &ECS, ticker: &Ticker) {
-    let tags = ecs.get_components::<PlayerTag>().unwrap();
-    let mut positions = ecs.get_components_mut::<PositionComponent>().unwrap();
-
-    let iter = tags
-        .iter()
-        .zip(positions.iter_mut())
-        .filter_map(|(tag, position)| Some((tag, position)));
-
-    for (tag, position) in iter {
-        println!("Tag: {:?} | Position: {:?}", tag, position);
-
-        if ticker.ticks % 120 == 0 {
-            position.x += 10;
-        }
-    }
-}
 
 pub struct AppState {
     world: ECS,
-    ticker: Ticker,
-
+    tick_state: TickState,
     gpu_state: GpuState,
 }
 
 impl AppState {
     pub fn new(window: Window) -> Self {
-        let mut world = ECS::new();
-        world.register_component::<PlayerTag>();
-        world.register_component::<PositionComponent>();
-
-        let player = world.create_entity();
-        world.set_component(player, PlayerTag);
-        world.set_component(player, PositionComponent::new(100, 100));
-
         Self {
-            world,
-            ticker: Ticker { ticks: 0 },
-
+            world: ECS::new(),
+            tick_state: TickState::new(60),
             gpu_state: GpuState::new(window),
         }
     }
 
     pub fn update(&mut self) {
-        self.ticker.ticks += 1;
+        for _ in 0..self.tick_state.update() {
+            println!("I'm ticking");
+        }
 
-        update_player(&self.world, &self.ticker);
+        std::thread::sleep(self.tick_state.tick_duration());
     }
 
     pub fn window(&self) -> &Window {
--- src/game/states/mod.rs
+++ src/game/states/mod.rs
@@ -1,2 +1,3 @@
 pub mod app_state;
 pub mod gpu_state;
+pub mod tick_state;
