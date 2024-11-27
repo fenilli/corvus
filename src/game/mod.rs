43a0fd959054888f2b22422dc78e283c1272e740
Gustavo Fenilli <gustavofenilli@gmail.com>
2024-11-27 14:02:04 -0300

    feat: adds simple example of player movement

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -4,6 +4,41 @@ use winit::window::Window;
 
 use crate::{clock::Clock, input::Input, world::World};
 
+#[derive(Debug)]
+struct TransformComponent {
+    position: (f64, f64),
+    rotation: f32,
+    scale: u32,
+}
+
+fn player_movement(world: &World, input: &Input, delta_time: f64) {
+    let player_speed = 100.0;
+
+    for transform in world
+        .get_components_mut::<TransformComponent>()
+        .unwrap()
+        .iter_mut()
+    {
+        if input.key_held(winit::keyboard::KeyCode::KeyA) {
+            transform.position.1 -= player_speed * delta_time;
+        }
+
+        if input.key_held(winit::keyboard::KeyCode::KeyD) {
+            transform.position.1 += player_speed * delta_time;
+        }
+    }
+}
+
+fn print_position(world: &World) {
+    for transform in world
+        .get_components_mut::<TransformComponent>()
+        .unwrap()
+        .iter_mut()
+    {
+        println!("x: {} y: {}", transform.position.0, transform.position.1);
+    }
+}
+
 pub struct Game {
     world: World,
     input: Input,
@@ -14,8 +49,21 @@ pub struct Game {
 
 impl Game {
     pub fn new(window: Window) -> Self {
+        let mut world = World::new();
+        world.register_component::<TransformComponent>();
+
+        let player = world.create_entity();
+        world.set_component(
+            player,
+            TransformComponent {
+                position: (100.0, 100.0),
+                rotation: 0.0,
+                scale: 1,
+            },
+        );
+
         Self {
-            world: World::new(),
+            world,
             input: Input::new(),
             clock: Clock::new(60),
 
@@ -25,8 +73,10 @@ impl Game {
 
     pub fn update(&mut self) {
         for delta_time in self.clock.update() {
-            println!("{}", delta_time);
+            player_movement(&self.world, &self.input, delta_time);
         }
+
+        print_position(&self.world);
     }
 
     pub fn input(&mut self) -> &mut Input {
